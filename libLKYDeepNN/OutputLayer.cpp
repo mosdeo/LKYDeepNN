6e47579ebb8d8ad6b2b91589ea8566e201ae2de8
林高遠 <lky@program.com.tw>
2017-01-03 22:39:02 +0800

    檢查倒傳遞演算法應該是沒問題

--- libLKYDeepNN/OutputLayer.cpp
+++ libLKYDeepNN/OutputLayer.cpp
@@ -3,42 +3,12 @@
 #include "HiddenLayer.hpp"
 #include "OutputLayer.hpp"
 
-// void OutputLayer::InitializeWeights()
-// {
-//     this->intoWeights = MakeMatrix(this->nodes.size(), this->previousLayer->nodes.size(), 1.0);
-//     this->intoBiases = vector<double>(this->nodes.size(), 0); //numNodes double with value 0
-//     this->wDelta = MakeMatrix(this->nodes.size(), this->previousLayer->nodes.size(), 0.0);
-//     this->bDelta = vector<double>(this->intoBiases.size());
-
-//     const double hi = 1/(sqrt(this->nodes.size()));
-//     const double lo = -hi;
-
-//     std::random_device rd;     // only used once to initialise (seed) engine
-//     std::mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
-//     std::uniform_real_distribution<double> uni_noise(lo, hi); // guaranteed unbiased
-
-//     for (size_t j = 0; j < this->intoWeights.size(); ++j)
-//     {
-//         for (size_t i = 0; i < this->intoWeights[j].size(); ++i)
-//         {
-//             this->intoWeights[j][i] = uni_noise(rng);
-//         }
-//     }
-
-//     for(double& bias : this->intoBiases)
-//     {
-//         bias = uni_noise(rng);
-//     }
-
-//     cout << "Completed output Layer InitializeWeights()" << endl;
-// }
-
 void OutputLayer::ForwardPropagation()
 {
     //將自己的節點歸零，因為要存放上一級傳來的運算結果，不能累積。
     this->nodes = vector<double>(this->nodes.size() ,0.0);
 
-    //節點的乘積與和
+    //節點的乘積與和 //[this][perv]
     for (size_t j = 0; j < this->nodes.size(); ++j) // compute i-h sum of weights * inputNodes
     {
         for (size_t i = 0; i < this->previousLayer->nodes.size(); ++i)
@@ -50,14 +20,14 @@ void OutputLayer::ForwardPropagation()
     }
 
     //活化函數
-    if(NULL == this->activation)
-    {
-        //cout << "WARNING: Output Layer 沒有配置活化函數，要做 Regression 嗎？" << endl;
-        //exit(EXIT_FAILURE);
-    }
-    else
+    if(NULL != this->activation)
     {//將自身節點全部跑一次活化函數
         this->nodes = this->activation->Forward(this->nodes);
+    }
+    else
+    {
+        //cout << "WARNING: Output Layer 沒有配置活化函數，要做 Regression 嗎？ 那應該配置 Linear" << endl;
+        exit(EXIT_FAILURE);
     }
 }
 
@@ -77,7 +47,7 @@ void OutputLayer::BackPropagation(double learningRate, vector<double> desiredOut
     //[this][perv]
     for(size_t j=0 ; j < this->wDelta.size() ; j++)
     {
-        double err = this->nodes[j] - desiredOutValues[j];//Output-target
+        double err = this->nodes[j] - desiredOutValues[j];//Output-target(Square Loss Function的微分)
         double derivativeActivation = this->activation->Derivative(this->nodes[j]);
 
         for(size_t i=0 ; i < this->wDelta[j].size() ; i++)
