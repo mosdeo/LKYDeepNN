153494ff8a3a52ec8a70c93aadf3abcf300e1425
林高遠 <lky@program.com.tw>
2017-01-03 21:32:05 +0800

    刪除 cast 註解

--- libLKYDeepNN/HiddenLayer.cpp
+++ libLKYDeepNN/HiddenLayer.cpp
@@ -6,43 +6,6 @@ using namespace std;
 
 int HiddenLayer::count = 0;
 
-// void HiddenLayer::SetActivation(Activation* activation)
-// {
-//     this->activation.reset(activation);
-// }
-
-// void HiddenLayer::InitializeWeights()
-// {   
-//     this->intoWeights = MakeMatrix(this->nodes.size(), this->previousLayer->NodesSize(), 1.0);
-//     this->intoBiases = vector<double>(this->nodes.size() ,0); //numNodes double with value 0
-//     this->wDelta = MakeMatrix(this->nodes.size(), this->previousLayer->NodesSize(), 0.0);
-//     this->bDelta = vector<double>(this->intoBiases.size());
-
-//     const double hi = 1/(sqrt(this->nodes.size()));
-//     const double lo = -hi;
-
-//     std::random_device rd;     // only used once to initialise (seed) engine
-//     std::mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
-//     std::uniform_real_distribution<double> uni_noise(lo, hi); // guaranteed unbiased
-
-//     for (size_t j = 0; j < this->intoWeights.size(); ++j)
-//     {
-//         for (size_t i = 0; i < this->intoWeights[j].size(); ++i)
-//         {
-//             this->intoWeights[j][i] = uni_noise(rng);
-//         }
-//     }
-
-//     for(double& bias : this->intoBiases)
-//     {
-//         bias = uni_noise(rng);
-//     }
-
-//     // cout << "  prev Layer: " << this->previousLayer->ToString() << endl;
-//     // cout << "  next Layer: " << this->nextLayer->ToString() << endl;
-//     // cout << "Completed hidden Layer InitializeWeights()" << endl;
-// }
-
 void HiddenLayer::ForwardPropagation()
 {
     // cout << "HiddenLayer::ForwardPropagation" << endl;
@@ -91,7 +54,6 @@ void HiddenLayer::BackPropagation(double learningRate)
         exit(EXIT_FAILURE);
     }
 
-
     //[this][perv]
     for(size_t j=0 ; j <  this->intoWeights.size() ; j++)
     {
@@ -99,11 +61,9 @@ void HiddenLayer::BackPropagation(double learningRate)
         double sigmaDeltaWeight = 0;
         for(size_t k=0;k<this->nextLayer->nodes.size();k++)
         {
-            //if(typeid(BackPropagationLayer*) == typeid(this->nextLayer))
-                sigmaDeltaWeight += this->nextLayer->wDelta[k][j] * dynamic_pointer_cast<BackPropagationLayer>(this->nextLayer)->intoWeights[k][j];
-            // else{
-            //     cout << "ERROR: this->nextLayer type error" << endl;
-            //     exit(EXIT_FAILURE);}
+            sigmaDeltaWeight +=
+                this->nextLayer->wDelta[k][j]*
+                dynamic_pointer_cast<BackPropagationLayer>(this->nextLayer)->intoWeights[k][j];
         }
 
         //此節點微分值
